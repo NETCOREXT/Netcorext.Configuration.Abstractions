2e43e3d104ec450e72285c903dc6b51c4522a7b9
Aren Chen <gucci313031@gmail.com>
2022-07-04 10:26:20 +0800

    ConnectionItemSection add username and password

--- ConfigSections/ConnectionItemSection.cs
+++ ConfigSections/ConnectionItemSection.cs
@@ -3,4 +3,6 @@ namespace Netcorext.Configuration.ConfigSections;
 public abstract class ConnectionItemSection
 {
     public string Connection { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
 }
