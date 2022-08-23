146d3fb267f0f1e948408bfc86d883d59838c5e7
Aren Chen <gucci313031@gmail.com>
2022-08-23 17:16:19 +0800

    add Key to CacheSection

--- ConfigSections/CacheSection.cs
+++ ConfigSections/CacheSection.cs
@@ -2,6 +2,7 @@ namespace Netcorext.Configuration.ConfigSections;
 
 public class CacheSection
 {
+    public string? Key { get; set; }
     public int? ServerDuration { get; set; }
     public int? ClientDuration { get; set; }
     public bool? ClientNoStore { get; set; }
