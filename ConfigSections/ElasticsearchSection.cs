80507d13e30dd886a87d1244fee1384be9b8ea2a
Aren Chen <gucci313031@gmail.com>
2023-05-06 18:22:20 +0800

    add elastic cloudId

--- ConfigSections/ElasticsearchSection.cs
+++ ConfigSections/ElasticsearchSection.cs
@@ -2,5 +2,6 @@ namespace Netcorext.Configuration.ConfigSections;
 
 public class ElasticsearchSection : ConnectionItemSection
 {
+    public string CloudId { get; set; }
     public string Index { get; set; }
 }
