8b6dfe26bd67de3ccec2d908487256a7e137038c
Aren Chen <gucci313031@gmail.com>
2024-01-12 08:47:43 +0800

    add EnableDetailedErrors、EnableSensitiveDataLogging、EnableServiceProviderCaching、EnableThreadSafetyChecks

--- ConfigSections/RelationalDbSection.cs
+++ ConfigSections/RelationalDbSection.cs
@@ -1,3 +1,9 @@
 namespace Netcorext.Configuration.ConfigSections;
 
-public class RelationalDbSection : ConnectionItemSection { }
+public class RelationalDbSection : ConnectionItemSection
+{
+    public bool? EnableDetailedErrors { get; set; }
+    public bool? EnableSensitiveDataLogging { get; set; }
+    public bool? EnableServiceProviderCaching { get; set; }
+    public bool? EnableThreadSafetyChecks { get; set; }
+}
